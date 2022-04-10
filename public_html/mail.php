21b6f13d42fcb72ae60c348d8c0e1ce5fe5b045d
Diana Djordjevic <angvil92@hotmail.com>
2022-04-10 17:52:05 +0200

    images are optimized and script files are now in one separate file

--- public_html/mail.php
+++ public_html/mail.php
@@ -5,15 +5,12 @@ $lastname=$_POST['prezime'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-
 if ($_POST['submit']) {
 
 $formcontent="From: $name \n $lastname \n Message: $message";
 $recipient = "office@dianadjordjevic.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-
-
 
 if(mail($recipient, $subject, $formcontent, $mailheader)){
 echo"<p>Your Message has been successfully sent!</p>";
